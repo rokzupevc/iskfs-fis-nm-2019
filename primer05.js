d1314ae8dd65b8877591b5e77f22e89087f60f66
rokzupevc <rokzupevc98@gmail.com>
2019-02-26 19:34:10 +0000

    primer 5

---
+++ primer05.js
@@ -0,0 +1,42 @@
+var http = require("http").createServer(handler);
+var firmata = require("firmata");
+var io = require("socket.io").listen(http); // knjižnica za vtičnike
+var fs = require("fs"); // knjižnica za delo z datotečnim sistemom ("file system fs")
+
+var board = new firmata.Board("/dev/ttyACM0", function(){// ACM (Abstract Control Model)
+                                                         // za serijsko komunikacijo z Arduinom (lahko je USB)
+    console.log("Priklop na Arduino");
+    board.pinMode(13, board.MODES.OUTPUT); // Posamezni pin konfiguriramo, da deluje kot vhod ali izhod
+    board.pinMode(8, board.MODES.OUTPUT); // Posamezni pin konfiguriramo, da deluje kot vhod ali izhod
+});
+
+function handler(req, res) { // "handler", ki je uporabljen pri require("http").createServer(handler)
+    fs.readFile(__dirname + "/primer05.html", // povemo, da bomo ob zahtevi ("request") posredovali
+    function (err, data) {                    // klientu datoteko primer04.html iz diska strežnika
+        if (err) {
+            res.writeHead(500, {"Content-Type": "text/plain"});
+            return res.end("Napaka pri nalaganju html strani.");
+        }
+    res.writeHead(200);
+    res.end(data);
+    });
+}
+
+http.listen(8080); // strežnik bo poslušal na vratih 8080
+
+io.sockets.on("connection", function(socket) {
+    socket.on("ukazArduinu", function(štUkaza) {
+        if (štUkaza == "1") {
+            board.digitalWrite(13, board.HIGH); // zapišemo +5V na pin 13
+        }
+        if (štUkaza == "0") {
+            board.digitalWrite(13, board.LOW); // zapišemo 0V na pin13
+        }
+        if (štUkaza == "3") {
+            board.digitalWrite(8, board.HIGH); // zapišemo +5V na pin 8
+        }
+        if (štUkaza == "2") {
+            board.digitalWrite(8, board.LOW); // zapišemo 0V na pin8
+        }
+    });
+});
