5e3b9d76e5b19a3e97392d4033bf770cb0800598
rokzupevc <rokzupevc98@gmail.com>
2019-02-26 20:23:00 +0000

    primer 11

---
+++ primer11.js
@@ -0,0 +1,48 @@
+var http = require("http").createServer(handler); // "on req" - "handler"
+var io = require("socket.io").listen(http); // socket knjižnica
+var fs = require("fs"); // spremenljivka za "file system" za posredovanje html
+var firmata = require("firmata");
+
+console.log("Starting the code");
+
+var board = new firmata.Board("/dev/ttyACM0", function(){
+    console.log("Priklop na Arduino");
+    console.log("Omogočimo Pin 0");
+    board.pinMode(0, board.MODES.ANALOG); // analogna nožica 0
+});
+
+function handler(req, res) {
+    fs.readFile(__dirname + "/primer11.html",
+    function (err, data) {
+        if (err) {
+            res.writeHead(500, {"Content-Type": "text/plain"});
+            return res.end("Napaka pri nalaganju strani.");
+        }
+    res.writeHead(200);
+    res.end(data);
+    })
+}
+
+var želenaVrednost = 0; // želena vrednost nastavljena s pot.
+
+http.listen(8080); // strežnik bo poslušal na vratih 8080
+
+board.on("ready", function() {
+    
+    board.analogRead(0, function(value){
+        želenaVrednost = value; // zvezno branje analogne nožice 0
+    });
+    
+    io.sockets.on("connection", function(socket) {
+        socket.emit("messageToClient", "Strežnik priključen, plošča pripravljena.");
+        setInterval(sendValues, 40, socket); // na 40ms pošljemo sporočilo klientu
+    }); // konec "sockets.on connection"
+
+}); // konec "board.on(ready)""
+
+function sendValues (socket) {
+    socket.emit("klientBeriVrednosti",
+    {
+    "želenaVrednost": želenaVrednost
+    });
+};
